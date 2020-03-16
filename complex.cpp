36d6deeb22d1a9657bd42b9d24fb50b9acb02994
Rares Bratan <bratan.rares@gmail.com>
2020-03-16 11:38:36 +0200

    am refacut putin header ul si antetele din .cpp

--- complex.cpp
+++ complex.cpp
@@ -4,28 +4,66 @@
 
 using namespace std;
 
+
+Complex::Complex() {
+    real = 0;
+    imag = 0;
+}
+
 Complex::Complex(double real, double imag) {
+    this->real = real;
+    this->imag = imag;
+}
+
+Complex::~Complex() = default;
+
+double Complex::getReal() const {
+    return real;
+}
+
+double Complex::getImag() const {
+    return imag;
+}
+
+void Complex::setReal(double newReal) {
+    this->real = newReal;
+}
+
+void Complex::setImag(double newImag) {
+    this->imag = newImag;
+}
+
+void Complex::show_compl() {
 
 }
 
-Complex Complex::add(Complex (*)(double, double)) const {
+void Complex::show_exp() {
+
+}
+
+Complex Complex::add(Complex other) const {
     return Complex(0, 0);
 }
 
-Complex Complex::mult(Complex (*)(double, double)) const {
+Complex Complex::mult(Complex other) const {
     return Complex(0, 0);
 }
 
-Complex Complex::quot(Complex (*)(double, double)) const {
+Complex Complex::quot(Complex other) const {
     return Complex(0, 0);
 }
 
-double Complex::abs(Complex (*)(double, double)) const {
+double Complex::abs(Complex other) const {
     return 0;
 }
 
-std::string Complex::compute_polar(Complex (*)(double, double)) const {
+std::string Complex::compute_polar(Complex other) const {
     return nullptr;
 }
+
+
+
+
+
 
 
--- complex.h
+++ complex.h
@@ -7,27 +7,39 @@ private:
     double real{}, imag{};
 
 public:
-    //constructor
+    //constructors
     Complex(double real, double imag);
+    Complex();
+
+    //destructor
+    ~Complex();
+
+    double getReal() const;
+    double getImag() const;
+
+    void setReal(double real);
+    void setImag(double real);
 
     //show _compl
+    void show_compl();
 
     //show_exp
+    void show_exp();
 
     //returns the addition of two complex numbers
-    Complex add(Complex(double r, double i)) const;
+    Complex add(Complex other) const;
 
     //returns the multiplication of two complex numbers
-    Complex mult(Complex(double r, double i)) const;
+    Complex mult(Complex other) const;
 
     //returns the division of two complex numbers
-    Complex quot(Complex(double r, double i)) const;
+    Complex quot(Complex other) const;
 
     //returns the absolute value of a complex number
-    double abs(Complex(double r, double i)) const;
+    double abs(Complex other) const;
 
     //returns the polar form of a complex number
-    std::string compute_polar(Complex(double r, double i)) const;
+    std::string compute_polar(Complex other) const;
 
 
 };
