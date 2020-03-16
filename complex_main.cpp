a055ac496032e8385ca8f49794272620255760c7
MariaBaba18 <61704335+MariaBaba18@users.noreply.github.com>
2020-03-16 15:11:48 +0200

    Add files via upload

--- complex_main.cpp
+++ complex_main.cpp
@@ -6,5 +6,19 @@ using namespace std;
 
 int main() {
 
+    //ex5
+    Complex v[5];
+    Complex obj1(3,4);
+    Complex obj2(5, 6);
+    Complex obj3(8,9);
+    Complex obj4(1,3);
+    Complex obj5(2,6);
+    v[0] = obj1;
+    v[1] = obj2;
+    v[2] = obj3;
+    v[3] = obj4;
+    v[4] = obj5;
 
+
+    return 0;
 }
